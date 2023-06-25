8c4dec8046681495aa268f1fdc546814827df385
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-06-25 21:36:47 +0000

    steal utils from anubis
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- src/bot/utils/exceptions.py
+++ src/bot/utils/exceptions.py
@@ -1,5 +1,7 @@
 """Custom Exception(s)"""
 
+from collections.abc import Hashable
+
 
 class APIError(Exception):
     """Raised when an external API (eg. Wikipedia) returns an error response."""
@@ -16,3 +18,22 @@ class MovedCommandError(Exception):
 
     def __init__(self, new_command_name: str):
         self.new_command_name = new_command_name
+
+
+class LockedResourceError(RuntimeError):
+    """
+    Exception raised when an operation is attempted on a locked resource.
+
+    Attributes:
+        `type` -- name of the locked resource's type
+        `id` -- ID of the locked resource
+    """
+
+    def __init__(self, resource_type: str, resource_id: Hashable):
+        self.type = resource_type
+        self.id = resource_id
+
+        super().__init__(
+            f"Cannot operate on {self.type.lower()} `{self.id}`; "
+            "it is currently locked and in use by another operation."
+        )
