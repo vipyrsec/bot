3f722bc9a660dc5a8999a2a12fbdd60b6c3b4c91
SwiftNathalie <swiftnathyz@gmail.com>
2023-06-30 18:33:41 +0000

    Fix suggestions

--- src/bot/exts/audit.py
+++ src/bot/exts/audit.py
@@ -4,11 +4,10 @@
 from .dragonfly._api import lookup_package_info
 from .dragonfly._api import PackageScanResult
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import discord
 from discord.ext import commands
-from discord.ext.commands import Cog, command, Context, Bot
-
+from discord.ext.commands import Cog, command, Context, Bot, Range
 
 
 class Audit(Cog):
@@ -22,28 +21,22 @@ class Audit(Cog):
     @commands.command()
     async def audit(self,
                     ctx: Context,
-                    since: datetime | int,
-                    package_amount: int = 50,
+                    since: Range[int, 1, 48],
+                    package_amount: Range[int, 1, 64] = 48
                     ) -> None:
         """Recalls for scanned packages within a given time frame and amount"""
 
-        hours = since if 0 < since < 48 else 24
-        since = datetime.now() - timedelta(hours=hours) 
+        since = datetime.now(tz=timezone.utc) - timedelta(hours=since) 
 
         packages: list[PackageScanResult] = await lookup_package_info(bot=self.bot, since=since)
         packages = packages[:package_amount] if len(packages) > package_amount else packages
         
         
-        embed = discord.Embed(title=f"Packages scanned in the last {hours} hours",
-                              description=f"Total packages scanned: {len(packages)}",
-                              )
-        embed.set_footer(text=f"Requested by {ctx.author.display_name}",
-                          icon_url=ctx.author.avatar_url,
-                          )
-        
+        embed = discord.Embed(title=f"Packages scanned in the last {hours} hours")
+        description = f"Total packages scanned: {len(packages)}\n\n"
+
         for package in packages:
-            embed.add_field(name=f'{package.name}@{package.version}',
-                            value=f'[Inspector URL]({package.inspector_url})',
-                            )
-            
+            description += f"[{package.name}@{package.version}]({package.inspector_url})\n"
+
+        embed.description = description
         await ctx.send(embed=embed)
