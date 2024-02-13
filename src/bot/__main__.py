91662494a0d639e1b1e0f94da2b75c066da8429c
Robin5605 <robinjefferson123@gmail.com>
2024-02-12 18:29:02 -0600

    Set up access to database

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -6,6 +6,7 @@ from collections.abc import Callable
 import discord
 from aiohttp import ClientSession, ClientTimeout
 from discord.ext import commands
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from bot import constants
 from bot.bot import Bot
@@ -27,24 +28,17 @@ def get_prefix(bot_: Bot, message_: discord.Message) -> Callable[[Bot, discord.M
 
 async def main() -> None:
     """Run the bot."""
+    database_engine = create_async_engine(constants.DatabaseConfig.url)
+    dragonfly_services = DragonflyServices(database_engine)
     async with ClientSession(headers={"Content-Type": "application/json"}, timeout=ClientTimeout(total=10)) as session:
         bot = Bot(
             guild_id=constants.Guild.id,  # type: ignore[arg-type]
             http_session=session,  # type: ignore[arg-type]
+            database_engine=database_engine,
+            dragonfly_services=dragonfly_services,
             allowed_roles=list({discord.Object(id_) for id_ in constants.MODERATION_ROLES}),  # type: ignore[arg-type]
             command_prefix=get_prefix,  # type: ignore[arg-type]
             intents=intents,  # type: ignore[arg-type]
-        )
-
-        bot.dragonfly_services = DragonflyServices(
-            session=session,
-            base_url=constants.Dragonfly.base_url,
-            auth_url=constants.Dragonfly.auth_url,
-            audience=constants.Dragonfly.audience,
-            client_id=constants.Dragonfly.client_id,
-            client_secret=constants.Dragonfly.client_secret,
-            username=constants.Dragonfly.username,
-            password=constants.Dragonfly.password,
         )
 
         await bot.start(constants.Bot.token)
